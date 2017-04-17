ccd862505402dcd1104d7bb7d42b8bf8c7db7e05
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:44:28 +0200

    tb_rhs_op_steel_rain.chernarus mission added

---
+++ briefing_blue.hpp
@@ -0,0 +1,8 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+
+<font size='14' color='#FFA500'>Attack Strategies:</font><br />
+* Utilize your MWS Supplies.<br />
+* Establish FO's quickly.<br />
+
+<font size='14' color='#FFA500'>Defense Strategies:</font><br />
+* Ensure you setup FO's before advancing to far.<br />
---
+++ briefing_red.hpp
@@ -0,0 +1,10 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+
+<font size='14' color='#FFA500'>Attack Strategies:</font><br />
+* Utilize your MWS Supplies.<br />
+* Establish FO's quickly.<br />
+
+<font size='14' color='#FFA500'>Defence Strategies:</font><br />
+* Ensure you setup FO's before advancing to far.<br />
+
+<font size='14' color='#FFA500'>Your side will get MI-24g after the capture of the third point</font><br />
